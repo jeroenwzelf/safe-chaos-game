303e8a4cda54612b4fa146288bfe938b88213e5f
Jeroen Donkers <j.donkers@emcatron.com>
2023-05-14 19:04:36 +0200

    Add drawing random points on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use coffee::graphics::{Color, Frame, Window, WindowSettings};
+use std::vec::Vec;
+use rand::Rng;
+use coffee::graphics::{Color, Frame, Mesh, Shape, Rectangle, Window, WindowSettings};
 use coffee::load::Task;
 use coffee::{Game, Result, Timer};
 
@@ -15,7 +17,22 @@ fn main() -> Result<()> {
     })
 }
 
+struct Point {
+    x: u32,
+    y: u32,
+}
+
+impl Point {
+    pub fn random() -> Self {
+        return Self {
+            x: rand::thread_rng().gen_range(0..SCREEN_WIDTH),
+            y: rand::thread_rng().gen_range(0..SCREEN_HEIGHT),
+        };
+    }
+}
+
 struct ChaosGame {
+    points: Vec<Point>,
 }
 
 impl Game for ChaosGame {
@@ -24,10 +41,22 @@ impl Game for ChaosGame {
 
     fn load(_window: &Window) -> Task<ChaosGame> {
         Task::succeed(|| ChaosGame {
+            points: Vec::new(),
         })
     }
 
     fn draw(&mut self, frame: &mut Frame, _timer: &Timer) {
         frame.clear(Color::BLACK);
+        self.points.push(Point::random());
+        for point in self.points.iter() {
+            let mut mesh = Mesh::new();
+            mesh.fill(Shape::Rectangle(Rectangle {
+                x: point.x as f32,
+                y: point.y as f32,
+                width: 1.0,
+                height: 1.0,
+            }), Color::WHITE);
+            mesh.draw(&mut frame.as_target());
+        }
     }
 }
